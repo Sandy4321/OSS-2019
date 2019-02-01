ae74b66851af6bf4bf162e08450410bfdaf7fa31
suporte <suporte@dataServer01.dacom>
2019-02-01 16:18:10 -0200

    Update telescope collector with request messages.

--- scripts/telescope/collector.py
+++ scripts/telescope/collector.py
@@ -21,42 +21,44 @@ class Collector:
         try:
             parameters['client_id'] = self.client_id
             parameters['client_secret'] = self.client_secret
-            url = 'https://api.github.com/' + path 
+            url = 'https://api.github.com/' + path
+            print('Creating request for: ' + url)
             response = requests.get(url, params=parameters, headers=headers)
             self.verify_rate_limit(response.headers)
             return response.json()
 
         except requests.exceptions.HTTPError as error:
             with open('exceptions.log', 'a') as exceptions:
-                exceptions.write('[HTTP] ' + error.text)                
+                exceptions.write('[HTTP] ' + error.text)
         except requests.exceptions.ConnectionError as error:
             with open('exceptions.log', 'a') as exceptions:
-                exceptions.write('[CONNECTION] ' + error.text)                
+                exceptions.write('[CONNECTION] ' + error.text)
         except requests.exceptions.Timeout as error:
             with open('exceptions.log', 'a') as exceptions:
-                exceptions.write('[TIMEOUT] ' + error.text)                
+                exceptions.write('[TIMEOUT] ' + error.text)
         except requests.exceptions.RequestException as error:
             with open('exceptions.log', 'a') as exceptions:
                 exceptions.write('[REQUEST] ' + error.text)
 
     def custom_request(self, url, parameters={}, headers={}, file_type='text'):
         try:
+            print('Creating request for: ' + url)
             response = requests.get(url, params=parameters, headers=headers)
 
             if file_type == 'json':
                 return response.json()
-            if file_type == 'text':    
+            if file_type == 'text':
                 return response.text
-                
+
         except requests.exceptions.HTTPError as error:
             with open('exceptions.log', 'a') as exceptions:
-                exceptions.write('[HTTP] ' + error.text)                
+                exceptions.write('[HTTP] ' + error.text)
         except requests.exceptions.ConnectionError as error:
             with open('exceptions.log', 'a') as exceptions:
-                exceptions.write('[CONNECTION] ' + error.text)                
+                exceptions.write('[CONNECTION] ' + error.text)
         except requests.exceptions.Timeout as error:
             with open('exceptions.log', 'a') as exceptions:
-                exceptions.write('[TIMEOUT] ' + error.text)                
+                exceptions.write('[TIMEOUT] ' + error.text)
         except requests.exceptions.RequestException as error:
             with open('exceptions.log', 'a') as exceptions:
                 exceptions.write('[REQUEST] ' + error.text)
@@ -64,7 +66,7 @@ class Collector:
     def verify_rate_limit(self, headers):
         self.rate_limit_remaining = int(headers['x-ratelimit-remaining'])
         self.rate_limit_reset = int(headers['x-ratelimit-reset'])
-                
+
         datetime_format = '%Y-%m-%d %H:%M:%S'
         reset_time = datetime.fromtimestamp(self.rate_limit_reset).strftime(datetime_format)
         current_time = datetime.now().strftime(datetime_format)
